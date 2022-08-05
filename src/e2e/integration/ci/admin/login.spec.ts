b02127b0410048561a0003abf3e1b6c084bc6da6
Matt Russell <matt.russell@glean.co>
2022-08-05 16:07:54 +0100

    Increase test iterations

--- src/e2e/integration/ci/admin/login.spec.ts
+++ src/e2e/integration/ci/admin/login.spec.ts
@@ -1,11 +1,11 @@
 import * as LogInForm from 'support/admin/pages/LogInForm'
 
 describe(`Log In Megatest`, () => {
-  for (let step = 1; step <= 200; step++) {
+  for (let step = 1; step <= 500; step++) {
     it(`Log In ${step}`, () => {
       cy.visitAdmin('/')
       LogInForm.verifyIsShown()
-      
+
       LogInForm.inputEmail("some.user@example.com")
       LogInForm.inputPassword('wrongpassword')
       LogInForm.clickLogIn()
