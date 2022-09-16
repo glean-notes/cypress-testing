2b4d2bbaab3686770bf236c31e4bae47ec087f83
Alyssa <alyssa.burlton@glean.co>
2022-09-16 09:41:39 +0100

    Run against trimmed down admin app

--- src/e2e/cypress.config.local.ts
+++ src/e2e/cypress.config.local.ts
@@ -4,13 +4,13 @@ import {defineConfig} from 'cypress'
 export default defineConfig({
   ...ciConfig,
   env: {
-    COOKIE_DOMAIN: 'cypress-testing-2.dev.glean.ninja',
-    ADMIN_URL: 'https://cypress-testing-2.dev.glean.ninja/admin',
-    GLEAN_URL: 'https://cypress-testing-2.dev.glean.ninja',
+    COOKIE_DOMAIN: 'notest-simplify-admin.dev.glean.ninja',
+    ADMIN_URL: 'https://notest-simplify-admin.dev.glean.ninja/admin',
+    GLEAN_URL: 'https://notest-simplify-admin.dev.glean.ninja',
     LOG_FAILURES_TO_KIBANA: 'false',
   },
   e2e: {
     ...ciConfig.e2e,
-    baseUrl: 'https://cypress-testing-2.dev.glean.ninja',
+    baseUrl: 'https://notest-simplify-admin.dev.glean.ninja',
   },
 })
